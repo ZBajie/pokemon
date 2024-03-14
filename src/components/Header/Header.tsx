ce35f652f11d0e6f3c87c7faf898889f2b95b704
Lars Abelsson <georg483@msn.com>
2024-03-14 16:04:18 +0100

    Header scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
 function Header() {
   return (
     <header className="header-main">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+        alt=""
+      />
       <h1>Pokemon</h1>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+        alt=""
+      />
     </header>
   )
 }
