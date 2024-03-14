7630eeb030f2d51679745ff9554a820a06c57402
Lars Abelsson <georg483@msn.com>
2024-03-14 08:52:12 +0100

    Pokemons

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header-main">
+      <h1>Pokemon</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Pokemons/Pokemons.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react"
+
+export type Root = {
+  count: number
+  next: string
+  previous: string
+  results: Result[]
+}
+
+export type Result = {
+  name: string
+  url: string
+}
+
+function Pokemons() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [pokemonsUrl, setPokemonsUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  )
+  const [pokemons, setPokemons] = useState({} as Root)
+
+  useEffect(() => {
+    let isMounted = true
+    const getPokemons = async () => {
+      try {
+        setIsLoading(true)
+        const result = await fetch(pokemonsUrl)
+        const data = await result.json()
+        if (isMounted) {
+          console.log(data)
+          setPokemons(data)
+        }
+      } catch (err) {
+        if (isMounted) {
+          console.log(err)
+        }
+      } finally {
+        if (isMounted) {
+          setIsLoading(false)
+        }
+      }
+    }
+    getPokemons()
+    return () => {
+      isMounted = false
+    }
+  }, [pokemonsUrl])
+  return (
+    <section className="pokemons">
+      <h2>Pokemons</h2>
+      <ul>
+        {isLoading ? (
+          <li>Loading...</li>
+        ) : (
+          pokemons.results &&
+          pokemons.results.map((item, i) => {
+            return <li key={i}>{item.name}</li>
+          })
+        )}
+      </ul>
+      <div className="buttons">
+        <button
+          onClick={() => {
+            setPokemonsUrl(pokemons.previous)
+          }}
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => {
+            setPokemonsUrl(pokemons.next)
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default Pokemons
